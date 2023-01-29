7446b30bdb5ac632a53f4a060e5ab19f1eaa8799
Ar Rafi Uddin <arrafi.mahin@gmail.com>
2023-01-30 01:48:36 +0600

    new place input added

--- src/Places/Pages/NewPlace.js
+++ src/Places/Pages/NewPlace.js
@@ -1,7 +1,18 @@
 import React from "react";
-
+import Input from "../../Shared/Components/FormElements/Input";
+import "./NewPlace.css";
 function NewPlace(props) {
-  return <div>new place</div>;
+  return (
+    <form className="place-form">
+      <Input
+        element="input"
+        type="text"
+        label="title"
+        validators={[]}
+        errorText="Please Enter a valid Title."
+      />
+    </form>
+  );
 }
 
 export default NewPlace;
